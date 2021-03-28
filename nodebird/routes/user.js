8bd95607f0aa7dbcb0e1fa3c8c55b5036852b174
jiyeon lee <a1919361@gmail.com>
2021-03-28 12:19:38 +0900

    팔로우끊기 실습

--- nodebird/routes/user.js
+++ nodebird/routes/user.js
@@ -15,4 +15,16 @@ router.post('/:id/follow', isLoggedIn, async (req,res,next)=> {
   }
 });
 
+router.post('/:id/unfollow', isLoggedIn, async (req,res,next)=> {
+  try{
+    const user = await User.findOne({where: {id: req.user.id}});
+    // a.removeB:a에 B관계 삭제
+    await user.removeFollowing(parseInt(req.params.id, 10))
+    res.send('sucess');
+  }catch(error){
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;

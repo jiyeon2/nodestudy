76a5946309d8a439cb4cedf52f5bbdbc4f85e290
jiyeon lee <a1919361@gmail.com>
2021-04-08 22:09:53 +0900

    socket.io 의 네임스페이스

--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,8 +1,51 @@
+const { default: axios } = require('axios');
 const SocketIO = require('socket.io');
 
 
 module.exports = (server) => {
   const io = SocketIO(server, {path: '/socket.io'});
+
+// 네임스페이스
+// 실시간 데이터가 전달될 주소를 구별할 수 있다
+// 기본값은 '/'
+const room = io.of('/room');
+const chat = io.of('/chat');
+
+room.on('connection', (socket) => {
+  console.log('room 네임스페이스에 접속');
+  socket.on('disconnection', () => {
+    console.log('room네임스페이스 접속 해제');
+  })
+})
+
+chat.on('connection', (socket) => {
+  console.log('chat네임스페이스에 접속');
+  const req = socket.request;
+  const {headers: {referer}} = req;
+  const roomId = referer
+  .split('/')[referer.split('/').length -1]
+  .replace(/\?.+/, '');
+  // req.headers.referer 은 이렇게 들어옴(방의 주소) -> /room/qkdwpahr
+
+  socket.join(roomId); // 방에 접속하는 코드
+
+  socket.to(roomId).emit('join',{ // 해당 방id에만 메시지 보냄
+    user:'system',
+    chat: `${req.session.color}님이 입장하셨습니다`
+  });
+  socket.on('disconnect', () => {
+    console.log('chat 네임스페이스 접속 해제');
+    socket.leave(roomId); // 방 나가기
+    const currentRoom = socket.adapter.rooms[roomId];
+    const userCount = currentRoom ? currentRoom.length : 0;
+    if (userCount === 0){
+      axios.delete(`http://localhost:8005/room/${roomId}`)
+      .then(() => {
+        console.log('방 제거 요청 성공')
+      }).catch(error => console.error(error));
+    }
+  })
+})
 
   io.on('connection', (socket) => {
     const req = socket.request;
